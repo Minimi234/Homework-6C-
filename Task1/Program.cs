468684133f8bda95fd6a387a0f43a9bd081c0acd
Anisa <An1sa_jon@mail.ru>
2022-09-03 15:26:00 +0400

    Tfsk41

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿void kolichestvoChisel(int[] number)
+{
+    int count = 0;
+    for(int i = 0; i < number.Length; i++)
+        if (number[i] > 0) count = count + 1;
+    Console.WriteLine(count);
+}
+
+int maxNumbers = 5;
+int[] myArray = new int[maxNumbers];
+for (int i = 0; i < maxNumbers; i++)
+{
+    Console.Write("Введите значения: ");
+    myArray[i] = Convert.ToInt32(Console.ReadLine());
+}
+    
+kolichestvoChisel(myArray);
